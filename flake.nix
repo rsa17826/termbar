da27e52052e2e42ccc4eab512aecdd51ddb4f1bb
nyix <rsa17826@email.vccs.edu>
2026-05-23 18:24:50 -0400

    NO MESSAGE SET

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          # The actual package
+          default = pkgs.buildGoModule {
+            pname = "termbar";
+            version = "1";
+            src = ./.;
+            vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          };
+        };
+        devShells = {
+          # Development environment
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+            ];
+          };
+        };
+      }
+    );
+}
